5d03c2fac1508bcd696d19d5a604787f0c29252f
“adash333” <adash333@hotmail.com>
2021-10-11 22:24:35 +0900

    Initial Commit

---
+++ Chord WatchKit Extension/ChordApp.swift
@@ -0,0 +1,19 @@
+//
+//  ChordApp.swift
+//  Chord WatchKit Extension
+//
+//  Created by a on 2021/10/11.
+//
+
+import SwiftUI
+
+@main
+struct ChordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
